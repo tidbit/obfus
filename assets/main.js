ba8bdc642df7acc1d3c8d6fad4f7ff5f8e4e9608
Matthew Simo <matthew.a.simo@gmail.com>
2015-04-28 10:53:48 -0500

    Update fill for down arrow
    Remove logs, trim and format some code down.
    
    Update svg code. Add info section & info toggle svgs
    
    Add info & info toggle svg styles. tweaks to header sizing & disable user-select on body
    
    Add info section events & toggling. Handle anchor tags opening in default browser.
    
    Add the question mark & close svg icons.
    
    clean up input style

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,9 @@
-console.log(document);
-
 var getElem = function( id ) { return document.getElementById(id); };
 
-
-console.log(getElem);
-
-var header = getElem("header"); 
-var app = getElem("app"); 
-var input = getElem("email-input");
-var obfus = getElem("obfuscated");
+var header = getElem("header"), 
+	app = getElem("app"), 
+	input = getElem("email-input"),
+	obfus = getElem("obfuscated");
 
 input.addEventListener( 'focus', function(e) {
 	header.classList.add("small");
@@ -16,11 +11,10 @@ input.addEventListener( 'focus', function(e) {
 });
 
 input.addEventListener( 'input', function(e) {
-	console.log(e);
-	var value = e.srcElement.value.trim();
-	obfus.value =	value.split("").map( function( character ) { return '&#' + character.charCodeAt(0) + ';'; } ).join("");
+	obfus.value =	e.srcElement.value.trim().split("").map( function( character ) {
+		return '&#' + character.charCodeAt(0) + ';'; 
+	}).join("");
 });
-
 
 var selectObfuscated = function() {
 	obfus.focus();
@@ -30,3 +24,43 @@ var selectObfuscated = function() {
 obfus.addEventListener( 'focus', selectObfuscated); 
 obfus.addEventListener( 'click', selectObfuscated);
 
+
+
+
+/**
+ * Handle hiding/showing info section
+ */
+var infoIsShown = false;
+var infoSVG = document.querySelectorAll("#info-toggle svg");
+var info = getElem("info");
+
+var toggleInfo = function( e ) {
+
+	for ( var i = 0; i < infoSVG.length; ++i ) {
+		infoSVG[i].classList.toggle('show');
+	}
+
+	info.classList.toggle('show');
+	infoIsShown = infoIsShown ? false : true;
+
+};
+
+for ( var i = 0; i < infoSVG.length; ++i ) {
+	infoSVG[i].addEventListener( 'click', toggleInfo );
+}
+
+
+/**
+ * Handle Opening Links in default browser
+ */
+var shell = require('shell');
+
+var openLink = function( e ) {
+	e.preventDefault();
+	shell.openExternal( e.srcElement.href );
+}
+
+var links = document.querySelectorAll("a");
+for ( var i = 0; i < links.length; ++i ) {
+	links[i].addEventListener( 'click', openLink );
+}
